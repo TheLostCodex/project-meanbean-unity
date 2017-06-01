b5e7efca598cd6ac69f895624a555f70f2328e3d
Dylan Tokita <dtokita@hawaii.edu>
2017-05-31 19:11:54 -1000

    First content push

---
+++ Project_Meanbean/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rbody;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movement != Vector2.zero) {
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat ("inputX", movement.x);
+			anim.SetFloat ("inputY", movement.y);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+
+		rbody.MovePosition (rbody.position + movement * 2 * Time.deltaTime);
+	}
+}
